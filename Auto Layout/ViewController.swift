5b6b506ee4f849ed3c33f91e5d1ba4f86031e809
Rodrigo Conte <rodrigo.conte14@gmail.com>
2020-07-15 16:26:01 -0400

    Initial Commit

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by Rodrigo Conte on 15/07/20.
+//  Copyright © 2020 Rodrigo Conte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
